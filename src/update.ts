cb203366aa908a56e3473a273d188d77c59e65d0
Masonlet <masonletoile@hotmail.com>
2026-05-16 13:08:59 -0400

    fix: clear frame inputs at the start of frame

--- src/update.ts
+++ src/update.ts
@@ -28,6 +28,9 @@ export function startLoop(
   let lastTime = performance.now() / 1000;
 
   function frame(nowMs: number) {
+    clearFrameKeyboard();
+    clearFramePointer();
+
     const now = nowMs / 1000;
     const elapsed = Math.min(now - lastTime, maxDelta);
     lastTime = now;
@@ -42,8 +45,6 @@ export function startLoop(
     }
 
     render();
-    clearFrameKeyboard();
-    clearFramePointer();
     reqId = requestAnimationFrame(frame);
   }
   reqId = requestAnimationFrame(frame);
